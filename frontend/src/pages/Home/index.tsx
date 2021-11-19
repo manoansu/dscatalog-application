c857a3279c9c9199c7a952eb6a7a06e1b37ec983
Ansumane Mané <manoansu@gmail.com>
2021-11-19 13:00:28 +0000

    Props

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
                 </div>
                 <div>
                   <Link to="/products">
-                    <ButtonIcon />
+                    <ButtonIcon text="START NOW YOUR SEARCH" />
                   </Link>
                 </div>
               </div>
