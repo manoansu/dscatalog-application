9a5f0936978c6a47754953b1ca11adeb5910bd25
Ansumane Mané <manoansu@gmail.com>
2021-11-22 13:06:50 +0000

    product details loader

---
+++ frontend/src/pages/ProductDetails/ProdctInfoLoader/index.tsx
@@ -0,0 +1,25 @@
+import ContentLoader from 'react-content-loader'
+
+const ProducInfoLoader = () => (
+  <ContentLoader 
+    viewBox="0 0 320 280" 
+    height={280} 
+    width={320}
+    backgroundColor="#f3f3f3"
+    foregroundColor="#acabab"
+    >
+    <rect x="0" y="0" rx="10" ry="10" width="280" height="180" />
+    <rect x="0" y="190" rx="0" ry="0" width="280" height="20" />
+    <rect x="0" y="215" rx="0" ry="0" width="239" height="20" />
+    <rect x="0" y="242" rx="0" ry="0" width="274" height="20" />
+  </ContentLoader>
+)
+
+ProducInfoLoader.metadata = {
+  name: 'RJavlonbek',
+  github: 'RJavlonbek',
+  description: 'Blog item',
+  filename: 'BlogItem',
+}
+
+export default ProducInfoLoader
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -6,6 +6,8 @@ import { Product } from 'types/products';
 import axios from 'axios';
 import { BASE_URL } from 'util/request';
 import { useEffect, useState } from 'react';
+import ProducInfoLoader from './ProdctInfoLoader';
+import ProducDetailsLoader from './ProductDetailsLoader';
 
 type UrlParams = {
     productId: string;
@@ -14,15 +16,20 @@ type UrlParams = {
 const ProductDatails = () => {
 
     const { productId } = useParams<UrlParams>();
+    const [isLoading, setIsLoading] = useState(false);
 
     // Declara o estado do componente usando o useState.
     const[product, setProduct] = useState<Product>();
 
     // Amarar o componente do react usando useEffect.
     useEffect(() => {
+        setIsLoading(true);
         axios.get(`${BASE_URL}/products/${productId}`)
         .then(response =>{
             setProduct(response.data)
+        })
+        .finally(() => {
+            setIsLoading(false);
         });
     },
     [productId]);
@@ -39,6 +46,8 @@ const ProductDatails = () => {
                 </Link>
                 <div className="row">
                     <div className="col-xl-6">
+                        { isLoading ?  ( <ProducInfoLoader /> ) :
+                            (<>
                         <div className="img-container">
                             <img src={product?.imgUrl}
                             alt={product?.name} />
@@ -47,12 +56,14 @@ const ProductDatails = () => {
                             <h1>{product?.name}</h1>
                             {product && <ProductPrice price={product?.price}/>}
                         </div>
+                        </>)}
                     </div>
                     <div className="col-xl-6">
-                        <div className="description-container">
+                        { isLoading ? (<ProducDetailsLoader />) : (
+                            <div className="description-container">
                             <h2>Product Description</h2>
                             <p>{product?.description}</p>
-                        </div>
+                        </div>)}
                     </div>
                 </div>
             </div>
