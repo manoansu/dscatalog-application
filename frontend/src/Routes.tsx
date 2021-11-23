e6f5452befb5b475fb19a9f7698009486fc116de
Ansumane Mané <manoansu@gmail.com>
2021-11-23 15:31:07 +0000

    Auth route

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,6 +4,7 @@ import Home from "pages/Home";
 import Catalog from "pages/Catalog";
 import { BrowserRouter, Switch,  Route, Redirect } from "react-router-dom";
 import ProductDatails from "pages/ProductDetails";
+import Auth from "pages/Admin/Auth";
 
 const Routes = () => (
     <BrowserRouter>
@@ -17,6 +18,10 @@ const Routes = () => (
             </Route>
             <Route path="/products/:productId">
                 <ProductDatails />
+            </Route>
+            <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+            <Route path="/admin/auth">
+                <Auth />
             </Route>
             <Redirect from="/admin" to="/admin/products" exact />
             <Route path="/admin">
