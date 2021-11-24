83739c2ff8dbccd01c14873aefa736956bef1804
Ansumane Mané <manoansu@gmail.com>
2021-11-24 12:03:08 +0000

    Login Request

--- frontend/src/pages/Admin/Login/index.tsx
+++ frontend/src/pages/Admin/Login/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import ButtonIcon from "components/ButtonIcon";
 import "./styles.css";
 import { useForm } from "react-hook-form";
+import { requestBackendLogin } from "util/request";
 
 type FormData ={
     username: string;
@@ -13,7 +14,14 @@ const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
 
     const onSubmit = (formData : FormData) =>{
-        console.log(formData);
+      requestBackendLogin(formData)
+        .then(response =>{
+          console.log('SUCESSO',response);
+        })
+        .catch(error =>{
+          console.log("ERROR", error);
+        });
+        
     };
 
   return (
--- frontend/src/util/request.ts
+++ frontend/src/util/request.ts
@@ -1,1 +1,31 @@
-export const BASE_URL = process.env.REACT_APP_BACEND_URL ?? 'http://localhost:8080';
+import axios from "axios";
+import qs from "qs";
+
+export const BASE_URL = process.env.REACT_APP_BACkEND_URL ?? 'http://localhost:8080';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+
+type LoginData = {
+    username: string;
+    password: string;
+}
+
+export const requestBackendLogin = (loginData : LoginData) =>{
+
+    // Dados de Headers no Postman que utilizamos para requisição no login de use, usando (Key and Value).
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+    }
+
+    // Dados de Body no Postman que utilizamos para requisição no login de user usando (Key and Value).
+    //E esse valor tem que ser convertido para urlencoded usando o qs.stringify que gera urlencode
+    // equivalente de objecto abaixo usando o sprédoperatorpor ex ...logindata,.
+    const data = qs.stringify ({
+        ...loginData,
+        grant_type : 'password'
+    });
+
+    return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
+}
