15ccdfe8fdc33b708d575ce8f44e190b8653d1c8
Ansumane Mané <manoansu@gmail.com>
2021-12-28 18:22:56 +0000

    product crud form HTML

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,9 +1,30 @@
-
+import './styles.css';
 
 const Form = () =>{
 
     return(
-        <h1>Tela de Form</h1>
+        <div className='product-crud-container'>
+            <div className="base-card product-crud-form-card">
+            <h1 className='product-crud-form-title'>DADOS DO PRODUTO</h1>
+            <form action="">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <input type="text" className='form-control base-input'/>
+                        <input type="text" className='form-control base-input'/>
+                        <input type="text" className='form-control base-input'/>
+                       
+                    </div>
+                    <div className="col-lg-6">
+                        <textarea name="" rows={10} className='form-control base-input'></textarea>
+                    </div>
+                </div>
+                <div >
+                    <div className="btn btn-outline-danger">CANCELAR</div>
+                    <div className="btn btn-primary">SALVAR</div>
+                </div>
+            </form>
+            </div>
+        </div>
     );
 };
 
